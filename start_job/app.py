3192ff48390d487bc3e605a9f2a05fd7fa94085a
alope107 <auberonlopez@gmail.com>
2023-10-19 18:00:01 -0700

    Add dummy job submitting logic.
    - Start job lambda now submits a dummy job when called.
    
    - Added environment variables to get job definition and queue.
    
    - Added requirements for boto3.
    
    - Added permissions to start_job for submit_job.
    
    - Added submit_job call.

--- start_job/app.py
+++ start_job/app.py
@@ -1,3 +1,19 @@
+import boto3
+import os
+
+session = boto3.session.Session()
+batch_client = session.client('batch')
+JOB_DEFINITION = os.environ.get("JOB_DEFINITION")
+JOB_QUEUE = os.environ.get("JOB_QUEUE")
+
+print(f"{JOB_DEFINITION=} {JOB_QUEUE=}")
+
+
 def lambda_handler(event, context):
-    print("Yay, I got called!")
+    print(f"{JOB_DEFINITION=} {JOB_QUEUE=}")
     print(event)
+    batch_client.submit_job(
+        jobDefinition=JOB_DEFINITION,
+        jobQueue=JOB_QUEUE,
+        jobName="dummy"  # TODO(auberon): Later get this from the event
+    )
