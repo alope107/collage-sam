ff8693db186ddfb64c206b7a01aeba4e7be5d107
alope107 <auberonlopez@gmail.com>
2023-10-31 15:48:01 -0700

    Update start_job to use bucket name.
    - Changed from using bucket ARN to bucket name for start_job. The bucket
      ARN was coming in in a strage state from the event, so bucket name is
      preferred.
    
    - Moved prefixes (input/, output/) to global constants. Will potentially
      make configurable in the future.
    
    - Added more logging.
    
    - Updated tests.

--- sam/tests/unit/test_start_job.py
+++ sam/tests/unit/test_start_job.py
@@ -10,5 +10,5 @@ def test_start_job_submits_a_job(s3_put_event):
     assert batch_call["jobQueue"] == "mock-job-queue"
     assert batch_call["jobName"] == "mock-id"
     assert batch_call["containerOverrides"] == {
-        "command": ["mock-bucket-arn", "input/mock-id"]
+        "command": ["mock-bucket-name", "mock-id", "input/", "output/"]
     }
